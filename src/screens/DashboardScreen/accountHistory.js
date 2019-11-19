4db8d0ccbb66d3d641250a15d575448ce64719af
John Delton Humilde <j.deltonhumilde@gmail.com>
2019-11-19 20:12:55 +0800

    account history

--- src/screens/DashboardScreen/accountHistory.js
+++ src/screens/DashboardScreen/accountHistory.js
@@ -17,32 +17,64 @@ const DATA = [
     id: '1',
     title: 'Payroll',
     date: '11-15-2019',
-    amount: 5950
+    amount: 5950.00
   },
   { 
     id: '2',
     title: 'ATM Withdrawal',
     date: '11-01-2019',
-    amount: 20000
+    amount: -20000.00
   },
   { 
     id: '3',
     title: 'ATM Withdrawal',
     date: '10-30-2019',
-    amount: 8000
+    amount: 8000.00
   },
   { 
     id: '4',
     title: 'Payroll',
     date: '10-15-2019',
-    amount: 14000
+    amount: 14000.00
+  },
+  { 
+    id: '5',
+    title: 'Payroll',
+    date: '10-1-2019',
+    amount: 14000.00
+  },
+  { 
+    id: '6',
+    title: 'ATM Withdrawal',
+    date: '10-5-2019',
+    amount: -19000.00
   }
 ]
 
-function Item({ title }) {
+numFixed = (amount) => {
+  amount = Math.abs(amount);
+  amount = amount.toFixed(2);
+
+  let str = amount.split('.');
+  if (str[0].length >= 4) {
+    str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+  }
+  if (str[1] && str[1].length >= 4) {
+      str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+  }
+  return str.join('.');
+}
+
+function Item({ title, date, amount }) {
   return (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
+    <View style={localStyles.listItem}>
+      <Text style={localStyles.itemText}>{title}</Text>
+      <Text style={localStyles.dateText}>{date}
+        
+      </Text>
+      <Text style={[localStyles.amountText, { color: Math.sign(amount) === -1 ? 'red' : 'green' }]}>
+          { Math.sign(amount) === -1 ? ('(PHP ' + this.numFixed(amount) + ')') : ('PHP ' + this.numFixed(amount)) }
+      </Text>
     </View>
   );
 }
@@ -72,7 +104,7 @@ class AccountHistoryScreen extends React.Component {
           <SafeAreaView style={localStyles.listStyle}>
             <FlatList
               data={ DATA }
-              renderItem={({ item }) => <Item title={item.title} />}
+              renderItem={({ item }) => <Item title={item.title} amount={item.amount} date={item.date} />}
               keyExtractor={ item => item.id } 
             />
           </SafeAreaView>
@@ -96,26 +128,67 @@ let localStyles = StyleSheet.create({
     color: '#f5ac14',
     fontFamily: 'Menlo-Bold',
     fontWeight: 'bold',
-    paddingTop: 30,
+    paddingTop: 20,
     fontSize: 20
   },
   subtitle : {
     textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 35,
-    color: '#fff'
+    fontSize: 65,
+    color: '#fff',
+    marginTop: 20,
+    paddingBottom: 10
   },
   bodyTitle : {
     textAlign: 'center',
     color: '#696969',
     fontFamily: 'Menlo-Bold',
     fontWeight: 'bold',
-    paddingTop: 30,
+    paddingTop: 20,
+    paddingBottom: 10,
     fontSize: 20
   },
   listStyle : {
     flex: 1,
-    marginTop: 10
+    marginTop: 10,
+  },
+  listItem : {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#fff',  
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  itemText : {
+    textAlign: 'left',
+    color: '#696969',
+    textTransform: 'uppercase',
+    fontFamily: 'Avenir-Light',
+    paddingLeft: 10,
+    paddingTop: 10,
+    fontSize: 20,
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  dateText : {
+    display: 'flex',
+    flexDirection: 'row',
+    textAlign: 'left',
+    color: '#696969',
+    fontFamily: 'Avenir-Light',
+    paddingLeft: 10,
+    paddingTop: 10,
+    fontSize: 15
+  },
+  amountText : {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    fontSize: 20,
+    textAlign: 'right',
+    marginLeft: 200,
+    paddingLeft: 5,
+    paddingRight: 10,
+    top: 30,
+    alignItems: 'flex-end' 
   }
 });
 
