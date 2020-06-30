37388552b47198957c49aea1db6a032302a5bf3a
Alvin Ching <alvin@thousandminds.com>
2020-06-30 23:20:20 +0800

    Added New Screens (Proof of Identity and Take a selfie)

---
+++ src/screens/TakeASelfie/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#122f5b",
+    flex: 1,
+  }
+});
+
+export default styles;
