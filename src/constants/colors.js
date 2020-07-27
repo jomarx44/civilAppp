959e2d8cdbf041dda6a78f77d95d2decdecaec26
Alvin Ching <alvin@thousandminds.com>
2020-07-27 16:25:28 +0800

    Updated Constants

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,2 +1,5 @@
 export const DEFAULT_PRIMARY_COLOR = "#F5AC14";
-export const DEFAULT_SECONDARY_COLOR = "#1e73be";
+export const DARK_BLUE = "#1e73be";
+export const LIGHT_BLUE = "#309FE7";
+export const DIRTY_WHITE = "#F7F7F7";
+export const WHITE = "#FFF";
---
+++ src/constants/lists.js
@@ -0,0 +1,8 @@
+export const bank = {};
+export const barangay = {};
+export const civilStatus = {};
+export const homeOwnership = {};
+export const idList = {};
+export const jobTitle = {};
+export const nationality = {};
+export const sourceOfFund = {};
