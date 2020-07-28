1ecd41fe9bea7f59897f75122716afc56892b502
Alvin Ching <alvin@thousandminds.com>
2020-07-28 15:05:01 +0800

    Updated Constants. Added account types constant.

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+export const constants = {
+  accountTypes: {
+    SA: "Savings Account",
+    LN: "Loan Accounts",
+    TD: "Time Deposit",
+  },
+};
--- src/constants/lists.js
+++
@@ -1,8 +0,0 @@
-export const bank = {};
-export const barangay = {};
-export const civilStatus = {};
-export const homeOwnership = {};
-export const idList = {};
-export const jobTitle = {};
-export const nationality = {};
-export const sourceOfFund = {};
